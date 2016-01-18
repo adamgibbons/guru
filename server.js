ef7545b8694c618e65edff123c3b0ab221b270cc
adamgibbons <adam.d.gibbons@gmail.com>
2016-01-17 19:27:02 -0800

    resolves CORS issue

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var restify = require('restify');
 var server = restify.createServer();
 var listDecks = require('./services/list-decks');
 
+server.use(restify.CORS({
+  credentials: true
+}));
+
 server.get('/decks', listDecks);
 
 server.listen(8080, function() {
